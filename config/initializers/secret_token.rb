ef8b79f9dbafb92797cb69ee3b7c8484e6b6125a
Grant Winter <grantrwinter@gmail.com>
2014-02-18 20:22:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dingtoggl::Application.config.secret_key_base = '4af6088edcb9e23f12644aa9635691dbe8f88c956f99fd6cd2e8653a5f68108a50438e3ff3088daf942b57b8a03243d72f2d44da43bfd17ea8544164cadfa8b5'
