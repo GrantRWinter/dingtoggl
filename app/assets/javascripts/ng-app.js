dcfa0602e8d82796dd35cd653f3c38473fdd2e72
Tom Sav <tomsav2010@gmail.com>
2014-02-19 19:50:50 +0000

    form for new entry (WIP)

--- app/assets/javascripts/ng-app.js
+++ app/assets/javascripts/ng-app.js
@@ -7,18 +7,11 @@ timeApp.controller("EntriesCtrl", function($scope, $resource) {
 	$scope.title = "Lighthouse TT App";
 	$scope.entries = Entry.query();
 	console.log($scope.data);
-	// $scope.data = {
-	// 	entries: [
-	// 		{ 
-	// 			project: "Project 1", 
-	// 			date: "12/01/2015",
-	// 			hours: 3.5,
-	// 			user_id: 1, 
-	// 			comment: "fdsafsa"
-	// 		}
-	// 	]
-	// }
-	// Entry.query(function(data) {
-	// 	console.log(data);
-	// });
+	$scope.newEntry = {};
+	
+	$scope.saveEntry = function() {
+		// use the Entry to POST an entry to the Rails app
+		alert('you tried to save an entry with comment: ' + $scope.newEntry.comment);
+	}
+
 });
