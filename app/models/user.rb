12b8bfdb8af1a9c96b2f552d292418242fbc62e1
Grant Winter <grantrwinter@gmail.com>
2014-03-03 23:04:26 +0000

    validation on email length added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,10 @@ class User < ActiveRecord::Base
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
+  validates :password, length: { in: 6..10}
   validates_presence_of :email
   validates_uniqueness_of :email
+
 end
 
 
